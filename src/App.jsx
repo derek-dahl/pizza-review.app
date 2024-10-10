5ef80f9c3e87f2ee3d920dbe0e90812d139e11bc
Derek Dahl <derekdahl@Dereks-MBP.directcom.com>
2024-10-09 20:54:58 -0600

    Change extensions to jsx for React

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import pizzaReviews from './data/pizzaReviews';
 import ReviewCard from './components/ReviewCard';
+import { useState } from 'react';
 
 
 const App = () => {
   return (
       <div className="container mt-4">
           <h1 className="text-center display-4 my-4">🍕 Pizza Reviews 🍕</h1>
+          <button className="btn btn-outline-primary mt-auto">Add Review</button>
           <div className="row">
               {pizzaReviews.map((review) => (
                   <div className="col-sm-6 col-md-4 mb-4" key={review.id}>
